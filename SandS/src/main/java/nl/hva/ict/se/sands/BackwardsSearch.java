e9e8766bc226665207bb2fdf1f5955116807220a
Thomas Bronsveld <Thomasbronsveld94@gmail.com>
2019-12-15 17:27:39 +0100

    Working implementation of backwards search

--- SandS/src/main/java/nl/hva/ict/se/sands/BackwardsSearch.java
+++ SandS/src/main/java/nl/hva/ict/se/sands/BackwardsSearch.java
@@ -1,6 +1,7 @@
 package nl.hva.ict.se.sands;
 
 public class BackwardsSearch {
+    private int amountOfComparisons = 0;
     /**
      * Returns index of the right most location where <code>needle</code> occurs within <code>haystack</code>. Searching
      * starts at the right end side of the text (<code>haystack</code>) and proceeds to the first character (left side).
@@ -10,7 +11,45 @@ public class BackwardsSearch {
      * character of the <code>needle</code>.
      */
     int findLocation(String needle, String haystack) {
+
+        int index = haystack.length() - needle.length();
+        boolean found = false;
+
+        System.out.println(haystack.charAt(index));
+        while(!found){
+
+            if(index < 0){
+                break;
+            }
+            char letter = haystack.charAt(index);
+
+            if(!needle.contains(Character.toString(letter))){
+                amountOfComparisons++;
+                index = index - needle.length();
+            } else if (needle.contains(Character.toString(letter))){
+                if(needle.charAt(0) != letter) {
+                    amountOfComparisons++;
+                    index--;
+                } else {
+                    for(int i = 0; i < needle.length(); i++){
+                        amountOfComparisons++;
+                        if(!(needle.charAt(i) == haystack.charAt(index + i))){
+                            index -= needle.length() - i;
+                            break;
+                        }
+                        if(i == needle.length() - 1){
+                            found = true;
+                            return index;
+                        }
+                    }
+                }
+            }
+        }
         return -1;
+    }
+
+    public void setAmountOfComparisons(int amountOfComparisons) {
+        this.amountOfComparisons = amountOfComparisons;
     }
 
     /**
@@ -18,7 +57,7 @@ public class BackwardsSearch {
      * @return the number of character comparisons during the last search.
      */
     int getComparisonsForLastSearch() {
-        return 0;
+        return amountOfComparisons;
     }
 
 }
